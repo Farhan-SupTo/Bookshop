da7afa31f2959d3063d3d4570715097197c82571
Your Name~ <you@example.com>
2023-11-02 21:44:20 +0600

    footer work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-      <footer className='px-4 divide-y  text-gray-800 relative bottom-0 top-6 left-0 sm:top-16'>
-        <div className='py-6 text-sm text-center text-gray-400'>
+      <footer className='px-4 divide-y  text-gray-800 relative bottom-0 top-6 left-0'>
+        <div className='py-6 text-lg text-center text-gray-400'>
           © 2023 nextPage Inc. All rights reserved.
         </div>
       </footer>
